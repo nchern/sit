bcbd1aea2216d6e866cb934956be95b2519dbc03
nchern <nchern@inbox.ru>
2021-08-09 23:12:50 +0200

    gen-open-issues-report.sh: refactor for better readability

--- bin/gen-open-issues-report.sh
+++ bin/gen-open-issues-report.sh
@@ -1,3 +1,10 @@
 #!/bin/sh -ue
 
-(printf "# Open issues\n\n\`\`\`\n"; sit  list|awk '/OPEN/ {$2="";$3=""; $4=""; print }'; printf "\`\`\`\n") > issues.md
+generate() {
+    printf "# Open issues\n\n\`\`\`\n"
+    sit  list | awk '/OPEN/ {$2="";$3=""; $4=""; print }'
+    printf "\`\`\`\n"
+}
+
+
+generate > issues.md
